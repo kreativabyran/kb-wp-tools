6c5d6b0af42919b51af8d377e8bebab5c96c7082
Oskar Modig <accounts@oskarmodig.com>
2022-06-20 11:24:52 +0200

    die on direct access

--- kb-wp-tools.php
+++ kb-wp-tools.php
@@ -5,4 +5,8 @@ Plugin URI: https://kreativabyran.se
 Description: Tools for configuring and debugging WordPress themes and plugins.
 */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
 require_once 'vendor/autoload.php';
--- setup.php
+++ setup.php
@@ -1,3 +1,12 @@
 <?php
+/**
+ * Autoloaded by composer to setup things by default.
+ *
+ * @package kreativabyran/kb-wp-tools
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
 
 KB_WP_Tools\Debug::setup();
